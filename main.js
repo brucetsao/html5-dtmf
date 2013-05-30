c683dcec120c4962ec6fe61d3d3b22cb5c5ce11c
Bjørn Tore Håvie <itsbth@itsbth.com>
2013-05-30 07:24:23 +0200

    Initial commit.

---
+++ main.js
@@ -0,0 +1,112 @@
+(function(){
+  var f, SineWave, ctx, tones, res$, i$, ref$, len$, k, DTMF, wait, delay, dial, nfield, dfield, button, slice$ = [].slice;
+  f = {
+    AudioContext: window.AudioContext || window.mozAudioContext || window.webkitAudioContext
+  };
+  SineWave = (function(){
+    SineWave.displayName = 'SineWave';
+    var prototype = SineWave.prototype, constructor = SineWave;
+    function SineWave(context, frequency){
+      this.context = context;
+      this.frequency = frequency != null ? frequency : 440;
+      this.x = 0;
+      this.sampleRate = this.context.sampleRate;
+      this.node = this.context.createJavaScriptNode(512, 1, 1);
+      this.node.onaudioprocess = bind$(this, 'process');
+      this.mod = this.sampleRate / (2 * Math.PI * this.frequency);
+      console.log(this.mod);
+    }
+    prototype.setFreq = function(frequency){
+      this.frequency = frequency;
+      return this.mod = this.sampleRate / (2 * Math.PI * this.frequency);
+    };
+    prototype.process = function(e){
+      var data, i$, to$, i;
+      data = e.outputBuffer.getChannelData(0);
+      for (i$ = 0, to$ = data.length; i$ < to$; ++i$) {
+        i = i$;
+        data[i] = Math.sin(this.x++ / this.mod);
+      }
+    };
+    prototype.play = function(){
+      this._active = true;
+      return this.node.connect(this.context.destination);
+    };
+    prototype.pause = function(){
+      if (this._active) {
+        this.node.disconnect();
+        return this._active = false;
+      }
+    };
+    return SineWave;
+  }());
+  ctx = new f.AudioContext;
+  res$ = {};
+  for (i$ = 0, len$ = (ref$ = [697, 770, 852, 941, 1209, 1336, 1477]).length; i$ < len$; ++i$) {
+    k = ref$[i$];
+    res$[k] = new SineWave(ctx, k);
+  }
+  tones = res$;
+  DTMF = {
+    H: [1209, 1336, 1477],
+    V: [697, 770, 852],
+    get: function(num){
+      if (num === 0) {
+        return [1336, 941];
+      }
+      return [DTMF.H[(num - 1) % 3], DTMF.V[~~((num - 1) / 3)]];
+    }
+  };
+  console.log("Loaded");
+  console.log(JSON.stringify(DTMF.get(5)));
+  wait = function(delay, fn){
+    return setTimeout(fn, delay);
+  };
+  delay = 40;
+  dial = function(num){
+    var tone, execute;
+    tone = function(n, next){
+      var ref$, t1, t2;
+      n = parseInt(n);
+      ref$ = DTMF.get(n), t1 = ref$[0], t2 = ref$[1];
+      console.log("Playing " + n + " (" + t1 + ", " + t2 + ") for " + delay + " ms");
+      tones[t1].play();
+      tones[t2].play();
+      return wait(delay, function(){
+        var _, ref$, v;
+        console.log("Pausing for " + delay + " ms");
+        for (_ in ref$ = tones) {
+          v = ref$[_];
+          v.pause();
+        }
+        return wait(delay, function(){
+          return next();
+        });
+      });
+    };
+    execute = function(arg$){
+      var first, rest;
+      first = arg$[0], rest = slice$.call(arg$, 1);
+      if (!first) {
+        return;
+      }
+      return tone(first, function(){
+        return execute(rest);
+      });
+    };
+    return execute(num.split(""));
+  };
+  nfield = document.querySelector("input[type=text]");
+  dfield = document.querySelector("input[type=number]");
+  button = document.querySelector("button");
+  button.addEventListener('click', function(e){
+    return dial(nfield.value);
+  });
+  dfield.value = delay;
+  dfield.addEventListener('change', function(e){
+    return delay = dfield.value;
+  });
+  function bind$(obj, key, target){
+    return function(){ return (target || obj)[key].apply(obj, arguments) };
+  }
+}).call(this);
---
+++ main.ls
@@ -0,0 +1,74 @@
+f =
+  AudioContext: window.AudioContext or window.mozAudioContext or window.webkitAudioContext
+
+class SineWave
+  (@context, @frequency=440) ->
+    @x = 0
+    @sample-rate = @context.sample-rate
+    @node = @context.createJavaScriptNode(512, 1, 1)
+    @node.onaudioprocess = @~process
+    @mod = @sample-rate / (2 * Math.PI * @frequency)
+    console.log(@mod)
+
+  setFreq: (@frequency) ->
+    @mod = @sample-rate / (2 * Math.PI * @frequency)
+
+  process: !(e) ->
+    data = e.outputBuffer.getChannelData(0)
+    for i from 0 til data.length
+      data[i] = Math.sin(@x++ / @mod)
+
+  play: ->
+    @_active = true
+    @node.connect(@context.destination)
+
+  pause: ->
+    if @_active
+      @node.disconnect()
+      @_active = false
+
+ctx = new f.AudioContext
+tones = {[k, new SineWave(ctx, k)] for k in [697 770 852 941 1209 1336 1477]}
+
+DTMF =
+  H: [1209 1336 1477]
+  V: [ 697  770  852]
+  get: (num) ->
+    return [1336 941] if num is 0
+    [ DTMF.H[(num - 1) % 3], DTMF.V[~~((num - 1) / 3)] ]
+
+console.log("Loaded")
+console.log(JSON.stringify(DTMF.get(5)))
+
+wait = (delay, fn) -> setTimeout fn, delay
+
+delay = 40
+
+dial = (num) ->
+  tone = (n, next) ->
+    n = parseInt(n)
+    [t1, t2] = DTMF.get(n)
+    console.log("Playing #n (#t1, #t2) for #delay ms")
+    tones[t1].play()
+    tones[t2].play()
+    <- wait(delay)
+    console.log("Pausing for #delay ms")
+    for _, v of tones
+      v.pause()
+    <- wait(delay)
+    next()
+  execute = ([first, ...rest]) ->
+    return unless first
+    <- tone(first)
+    execute(rest)
+  execute(num.split(""))
+
+nfield = document.querySelector("input[type=text]")
+dfield = document.querySelector("input[type=number]")
+button = document.querySelector("button")
+button.addEventListener \click, (e) ->
+  dial(nfield.value)
+
+dfield.value = delay
+dfield.addEventListener \change, (e) ->
+  delay := dfield.value
